bb152a7d2bb75e1b754d3016c9222b7a5367a452
githubmaidou <maidou.t00ls@gmail.com>
2016-05-11 01:37:21 +0800

    update

--- GoogleKeyword.py
+++ GoogleKeyword.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python
-BDUrl = "http://baidu.com/s?wd="
-BDPage = '&pn='
-BDkeyword = "youtube"
-BDOutFile = "./GSF.log"
+import re,urllib
+from bs4  import BeautifulSoup
+def BDSS(keyword,num):
+	BDUrl = "http://baidu.com/s?wd=%s&pn=%s" %(keyword,num*10)
+	return urllib.urlopen(BDUrl).read()
+keyword='youtube'
+for num in range(76):
+	soup=BeautifulSoup(BDSS(keyword,num))
+	obj=soup.find_all("a",class_="c-showurl")
+	for i in obj:
+		print  i.text
+
+
 
 
 
