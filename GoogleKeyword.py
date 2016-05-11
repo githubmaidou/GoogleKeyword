a5849d633d5214542d8cc17711edebf5b9892c98
root <root@localhost.localdomain>
2016-05-11 06:34:36 -0400

    update baidusearch

--- GoogleKeyword.py
+++ GoogleKeyword.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
-import re,urllib
+import re,urllib,socket
 from bs4  import BeautifulSoup
+socket.setdefaulttimeout(5)
 def BDSS(keyword,num):
-	BDUrl = "http://baidu.com/s?wd=%s&pn=%s" %(keyword,num*10)
+	BDUrl = "https://baidu.com/s?wd=%s&pn=%s" %(keyword,num*10)
 	return urllib.urlopen(BDUrl).read()
-keyword='youtube'
-for num in range(76):
+keyword='site:gov.cn'
+for num in range(5):
 	soup=BeautifulSoup(BDSS(keyword,num))
 	obj=soup.find_all("a",class_="c-showurl")
 	for i in obj:
-		print  i.text.encode('utf8')
-		print  i.text.split('/')[0]
-		print  "--------------------"
+		print BeautifulSoup(urllib.urlopen(i.get('href')).read()).find_all('noscript')[0].find_all('meta')[0].get('content').split(';')[1].split('\'')[1]
 
 
 
